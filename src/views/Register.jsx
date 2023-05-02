9ea4a7b033f8cb41bb46368d6f6926793cd26c9e
KAMBONO <kambonoj@gmail.com>
2023-05-02 13:27:30 +0300

    Connected the front to back with axios client

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import Button from '../components/Button'
 import axiosClient from '../axios-client';
 import { useStateContext } from '../components/contexts/ContextProvider'
@@ -8,8 +8,10 @@ import { useStateContext } from '../components/contexts/ContextProvider'
 export default function Register() {
   const nameRef = useRef();
   const emailRef = useRef();
+  const typeRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmationRef = useRef();
+  const [errors, setErrors] = useState()
 
   const {setUser,setToken} = useStateContext()
 
@@ -18,19 +20,23 @@ export default function Register() {
     const payload = {
       name: nameRef.current.value,
       email: emailRef.current.value,
+      type: typeRef.current.value,
       password: passwordRef.current.value,
       password_confirmation: passwordConfirmationRef.current.value,
     }
     console.log(payload);
-    axiosClient.post('/signup',payload).then(({data})=>{
+    axiosClient.post('/register',payload).then(({data})=>{
+      console.log(data)
       setUser(data.user)
       setToken(data.token)
     }).catch
     (err=>{
       const response = err.response;
       if(response && response.status === (422 || 500 || 404) ){
-        console.log(response.data.errors);
-      }
+        console.log(response.data.errors)
+        setErrors(response.data.errors)
+
+      };
     })
 
 
@@ -38,11 +44,32 @@ export default function Register() {
   return (
     <>
     <form align="center" onSubmit={onSubmit} method="post">
-    <h2 align="center"> Register Form</h2>
+    <h2 align="center">Register Now For Free</h2>
+      {
+        errors && 
+        <div className='alert'>
+          {
+            errors &&
+            <div className="alert">
+              {
+                Object.keys(errors).map(key =>(
+                  <h2 align="center" key={key}>
+                    {errors[key][0]}
+                  </h2>
+                ))
+              }
+
+
+            </div>
+            
+          }
+        </div>
+      }
 
       <div className="form-control">
-          <input ref={nameRef} type="text" placeholder='fullname'/>
+          <input ref={nameRef} type="text" placeholder='FullName'/>
           <input ref={emailRef} type="email" placeholder='Enter Email'/>
+          <input ref={typeRef} type="text" placeholder='Type of user?[ HOspitality || Other]'/>
           <input ref={passwordRef} type="password" placeholder='Password'/>
           <input ref={passwordConfirmationRef} type="password" placeholder='Password Confirmation'/>
       </div>
