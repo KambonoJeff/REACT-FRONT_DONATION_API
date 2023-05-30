10f161d82115c10c91d4cbe0b45146e3a6e49db8
KAMBONO <kambonoj@gmail.com>
2023-05-30 09:29:14 +0300

    Form development implementation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,9 +3,7 @@ import InputDetail from './InputDetail'
 import Button from './Button'
 import {useRef, useState} from 'react'
 
-const Form = ({  }) => {
-
-
+const Form = () => {
   const Hospitality = ()=>{
     const user_idRef = useRef();
     const foodRef = useRef();
@@ -32,14 +30,16 @@ const Form = ({  }) => {
       <>
       <h2> Hospitality Based institutions</h2>
      
-        <form action="" method="post">
-
+        <form action="" onSubmit={Hospitality} method="post">
+          <input ref={user_idRef} type="text" name="user_id" id="user_id" placeholder='Enter The User Id'/>
+          <input ref={} type="email" name="email" id="email" />
           <InputDetail ref={user_idRef} label='user_id' placeh='user_id'/>
           <InputDetail ref={foodRef} label='Type of Food' placeh='Fruits, Legumes,cereals, proteins,vitamins'/>
           <InputDetail ref={quantityRef} label='Quantity' placeh='Quantity '/>
           <InputDetail ref={beneficiariesRef} label='beneficiaries ' placeh='beneficiaries '/>
           <InputDetail ref={locationRef} label='location ' placeh='location '/>
           <InputDetail ref={statusRef} label='status ' placeh='status '/>
+
           <button type="submit" onClick={Hospitality()} className="btn"> POST</button>
         </form>
 
@@ -57,94 +57,94 @@ export default Form
 
 
 
-const Login=()=>{
-  return(
-    <>
-       <br></br>
-      <h2>login form </h2>      
-      <form action="" method="post">
-        <InputDetail label='Email' placeh='Email'/>
-        <InputDetail label='Password' placeh='Password' type='password'/>
-        <Button text='Login'/>
-      </form>
-    </>
+// const Login=()=>{
+//   return(
+//     <>
+//        <br></br>
+//       <h2>login form </h2>      
+//       <form action="" method="post">
+//         <InputDetail label='Email' placeh='Email'/>
+//         <InputDetail label='Password' placeh='Password' type='password'/>
+//         <Button text='Login'/>
+//       </form>
+//     </>
  
-  )        
-}
-const ngoLogin=()=>{
-  return(
-    <>
-       <br></br>
-      <h2>login form </h2>      
-      <form action="" method="post">
-        <InputDetail label='Email' placeh='Email'/>
-        <InputDetail label='licenseNo' placeh='licenseNo' type='password'/>
-        <Button text='Login'/>
-      </form>
-    </>
+//   )        
+// }
+// const ngoLogin=()=>{
+//   return(
+//     <>
+//        <br></br>
+//       <h2>login form </h2>      
+//       <form action="" method="post">
+//         <InputDetail label='Email' placeh='Email'/>
+//         <InputDetail label='licenseNo' placeh='licenseNo' type='password'/>
+//         <Button text='Login'/>
+//       </form>
+//     </>
  
-  )        
-}
-const register =()=>{
-  return(
+//   )        
+// }
+// const register =()=>{
+//   return(
     
-  <>
-  <br></br>
-    <h2> register form</h2>
+//   <>
+//   <br></br>
+//     <h2> register form</h2>
  
-  <form action="" method="post">
+//   <form action="" method="post">
 
-    <InputDetail label='fullname' placeh='First Name'/>
+//     <InputDetail label='fullname' placeh='First Name'/>
 
-    <InputDetail label='Email' placeh='Email' type='email'/>
-    <InputDetail label='Password' placeh='Password' type='password'/>
-    <InputDetail label='Confirm password' placeh='Confirm password' type='password'/>
-    <Button text='Register'/>
-  </form>    
-</> 
-  )
-}
-const ngoRequest=()=>{
-  return(
-    <>
-    <br></br>
-    <h2> Food Requesting Form</h2>
+//     <InputDetail label='Email' placeh='Email' type='email'/>
+//     <InputDetail label='Password' placeh='Password' type='password'/>
+//     <InputDetail label='Confirm password' placeh='Confirm password' type='password'/>
+//     <Button text='Register'/>
+//   </form>    
+// </> 
+//   )
+// }
+// const ngoRequest=()=>{
+//   return(
+//     <>
+//     <br></br>
+//     <h2> Food Requesting Form</h2>
 
    
-    <form action="" method="post">
-    <InputDetail label='user_id' placeh='user_id'/>
-        <InputDetail label='Type of Food' placeh='Fruits, Legumes,cereals, proteins,vitamins'/>
-        <InputDetail label='Quantity' placeh='Quantity '/>
-        <InputDetail label='beneficiaries ' placeh='beneficiaries '/>
-        <InputDetail label='location ' placeh='location '/>
-        <InputDetail label='status ' placeh='status '/>
-      <Button text='Request'/>
-    </form>
+//     <form action="" method="post">
+//     <InputDetail label='user_id' placeh='user_id'/>
+//         <InputDetail label='Type of Food' placeh='Fruits, Legumes,cereals, proteins,vitamins'/>
+//         <InputDetail label='Quantity' placeh='Quantity '/>
+//         <InputDetail label='beneficiaries ' placeh='beneficiaries '/>
+//         <InputDetail label='location ' placeh='location '/>
+//         <InputDetail label='status ' placeh='status '/>
+//       <Button text='Request'/>
+//     </form>
 
-  </>
+//   </>
 
-  )
-}
+//   )
+// }
 
-const ngoRegister =()=>{
-  return(
-    <>
-    <br></br>
-    <h2>NGO- Register-Form</h2>
+// const ngoRegister =()=>{
+//   return(
+//     <>
+//     <br></br>
+//     <h2>NGO- Register-Form</h2>
      
-      <form action="" method="post">
-        <InputDetail label='Organisation name' placeh='Organisation Name'/>
-        <InputDetail label='Email' placeh='Email'/>
-        <InputDetail label='Location' placeh='Location'/>
-        <InputDetail label='Total Number of Beneficiaries' placeh='Number'/>
-        <InputDetail label='Consumption Amount Per Week' placeh='Consumption Amount Per Week'/>
-        <InputDetail label='Phonenumber' placeh='Phonenumber'/>
-        <InputDetail label='licenseNo' placeh='licenseNo'/>
-        <Button text='POST'/>
+//       <form action="" method="post">
+//         <InputDetail label='Organisation name' placeh='Organisation Name'/>
+//         <InputDetail label='Email' placeh='Email'/>
+//         <InputDetail label='Location' placeh='Location'/>
+//         <InputDetail label='Total Number of Beneficiaries' placeh='Number'/>
+//         <InputDetail label='Consumption Amount Per Week' placeh='Consumption Amount Per Week'/>
+//         <InputDetail label='Phonenumber' placeh='Phonenumber'/>
+//         <InputDetail label='licenseNo' placeh='licenseNo'/>
+//         <Button text='POST'/>
 
-      </form>
+//       </form>
   
-  </>
-  )
+//   </>
+//   )
   
-}
+// }
