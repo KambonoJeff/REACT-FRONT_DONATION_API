70396130810fee8a1a4306fe2d25794389134555
KAMBONO <kambonoj@gmail.com>
2023-06-15 21:45:27 +0300

    Add Request Button Development

--- src/components/editForm/Ngo.jsx
+++ src/components/editForm/Ngo.jsx
@@ -1,19 +1,24 @@
 import React, { useRef } from 'react'
 import axiosClient from '../../axios-client';
+import { Link } from 'react-router-dom';
 
 export default function Ngo() {
   const nameRef = useRef();
   const emailRef = useRef();
   const locationRef = useRef();
   const beneficiariesRef = useRef();
+  const phonenumberRef = useRef();
+  const licenseNoRef = useRef();
 
   const onSubmit = (event)=>{
     event.preventDefault()
-    payload={
+  const payload={
       name : nameRef.current.value,
       email : emailRef.current.value,
       location : locationRef.current.value,
       beneficiaries : beneficiariesRef.current.value,
+      phonenumber : phonenumberRef.current.value,
+      licenseNo : licenseNoRef.current.value,
     }
     console.log(payload)
     axiosClient.post('/ngo/register').then((res)=>{console.log(res)}).catch((err)=>{
@@ -25,7 +30,7 @@ export default function Ngo() {
     <br />
     <br />
 
-    
+    <div className="brd">
             <h2> Add NGO </h2>
      
      <form action="" onSubmit={onSubmit} method="post">
@@ -48,10 +53,19 @@ export default function Ngo() {
        <input type="text" name="beneficiaries" id="beneficiaries" placeholder='beneficiaries' ref={beneficiariesRef} />
 
        </div>
+       <div className="form-control">
+       <input type="text" name="phonenumber" id="phonenumber" placeholder='phonenumber' ref={phonenumberRef} />
+
+       </div>
+       <div className="form-control">
+       <input type="text" name="licenseNo" id="licenseNo" placeholder='licenseNo' ref={licenseNoRef} />
+
+       </div>
   
        <button align='center' type="submit"  className="btn"> Add Ngo </button>
        <Link className='btn' to='/users/requests'>Back</Link>
      </form>
+     </div>
     </>
   )
 }
