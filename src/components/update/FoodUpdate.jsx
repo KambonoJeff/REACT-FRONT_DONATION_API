4cdf0445a7cc8f84490593762be24ab1373537d4
KAMBONO <kambonoj@gmail.com>
2023-06-20 20:03:56 +0300

    Update Functionality

--- src/components/update/FoodUpdate.jsx
+++ src/components/update/FoodUpdate.jsx
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import axiosClient from '../../axios-client';
 
 export default function FoodUpdate() {
     const {id} = useParams();
     const [load,setLoad] = useState(false);
-
+    let navigate = useNavigate();
     const [user,setUser] =useState({
-      user_id:null,
-      typeoffood:'',
-      quantity:'',
-      beneficiaries:'',
-      location:'',
-      status:''
+      id:null,
+      cereals:'',
+      proteins:'',
+      legumes:'',
+      breakfast:'',
+      snacks:'',
+      cash:''
     });
      if(id){
         useEffect(()=>{
@@ -43,8 +44,15 @@ export default function FoodUpdate() {
         {load &&  <h3 align='center'>Loading . . . </h3>}
         <form onSubmit={onSubmit}>
             <div className="form-control">
-            input
+            <input placeholder='cereals' value={user.cereals} onChange={event => setUser({...user , cereals: event.target.value})} />
+            <input placeholder='proteins' value={user.proteins} onChange={event => setUser({...user , proteins: event.target.value})} />
+            <input placeholder='legumes' value={user.legumes} onChange={event => setUser({...user , legumes: event.target.value})} />
+            <input placeholder='breakfast' value={user.breakfast} onChange={event => setUser({...user , breakfast: event.target.value})} />
+            <input placeholder='snacks' value={user.snacks} onChange={event => setUser({...user , snacks: event.target.value})} />
+            <input placeholder='cash' value={user.cash} onChange={event => setUser({...user , cash: event.target.value})} />
             <button className='btn' align='center'> SAVE </button>
+            
+            <button className='btn' onClick={()=>navigate(-1)}> Back </button>  
             </div>
         
         
