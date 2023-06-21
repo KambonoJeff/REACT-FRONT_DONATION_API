354cff5c30a4d05af60c3ece0079f8329e1e2413
KAMBONO <kambonoj@gmail.com>
2023-06-21 21:53:29 +0300

    Update functionality NGO table

--- src/components/update/NgoUpdate.jsx
+++ src/components/update/NgoUpdate.jsx
@@ -12,13 +12,35 @@ export default function NgoUpdate() {
         beneficiaries:'',
 
     }
-    const [load ,setLoad]= useState();
+    const [load ,setLoad]= useState(false);
     if(id){
-        axiosClient.get('/ngo')
+        setLoad(true)
+        axiosClient.get(`/showngo/${id}`).then(({data})=>{
+            setLoad(false)
+            console.log(data)
+        }).catch((err)=>{
+            setLoad(false)
+            console.log(err)
+        })
     }
   return (
     <>
-      
+      <br />
+        {ngo.id && <h2 align='center'> Ngo Name:  {ngo.name}</h2>}
+        {load &&  <h3 align='center'>  Loading . . . </h3>}
+        <form onSubmit={onSubmit}>
+            <div className="form-control">
+            <input placeholder='cereals' value={ngo.cereals} onChange={event => setngo({...ngo , cereals: event.target.value})} />
+            <input placeholder='proteins' value={ngo.proteins} onChange={event => setngo({...ngo , proteins: event.target.value})} />
+            <input placeholder='legumes' value={ngo.legumes} onChange={event => setngo({...ngo , legumes: event.target.value})} />
+            <input placeholder='breakfast' value={ngo.breakfast} onChange={event => setngo({...ngo , breakfast: event.target.value})} />
+            <input placeholder='snacks' value={ngo.snacks} onChange={event => setngo({...ngo , snacks: event.target.value})} />
+            <button className='btn' align='center'> SAVE </button>
+            <Link to='/ngos/ngo' className='btn'> Back </Link> 
+            </div>
+        
+        
+        </form>
 
 
     </>
