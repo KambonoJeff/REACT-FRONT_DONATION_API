3d9c9cdd11f9eb988819bcf109ce806dc721c2c6
KAMBONO <kambonoj@gmail.com>
2023-07-01 12:15:08 +0300

    Admin front login form function development

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,58 @@
-import React from 'react'
-import axiosClient from '../axios-client';
-import { useEffect,useState } from 'react';
 import { useStateContext } from './contexts/ContextProvider';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
+import React from 'react'
 
-const Header = ({  onLogout }) => {
+export default function Header({ onLogout }) {
   const {
     user,
     admin,
     ngo,
   }=useStateContext();
 
+  const SelectOccupied=(  res )=>{
+    let [d1,setD1] = useState();
+    let [d2, setD2] = useState();
+    let [ d3,setD3] = useState();
+
+    const getData = useEffect(()=>{
+      if(user){
+        setD1(user);
+        return d1;
+      }
+      if(admin){
+        setD2(admin);
+        return d2;
+  
+      }
+      if(ngo){
+        setD3(ngo);
+        return d3 ;
+  
+      }
+    },[])
+    
+    res = getData();
+
+  }
+
   return (
     <header>
         
         <div>
             <ul className="flex box">
-                <li > <h4> user name</h4></li>
+                <li > <h4> { SelectOccupied(res) } </h4></li>
                 <li ><a href="#" onClick={onLogout} className="btn">Logout</a></li>
             </ul>
         </div>
       
     </header>
   )
+
+
 }
 
-export default Header
+
+
+
