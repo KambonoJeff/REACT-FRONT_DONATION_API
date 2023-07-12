a2ee9b8ef4c845e9df8e644a684b44839f6a7ea2
KAMBONO <kambonoj@gmail.com>
2023-07-12 20:02:18 +0300

    State context variables compartmentalization

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,7 @@
 import { useStateContext } from './contexts/ContextProvider';
-import { useState } from 'react';
-import { useEffect } from 'react';
 
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function Header({ onLogout }) {
   const {
@@ -10,39 +9,18 @@ export default function Header({ onLogout }) {
     admin,
     ngo,
   }=useStateContext();
+  // let navigation = useNavigate();
 
-  const SelectOccupied=(  res )=>{
-    let [d1,setD1] = useState();
-    let [d2, setD2] = useState();
-    let [ d3,setD3] = useState();
-
-    const getData = useEffect(()=>{
-      if(user){
-        setD1(user);
-        return d1;
-      }
-      if(admin){
-        setD2(admin);
-        return d2;
-  
-      }
-      if(ngo){
-        setD3(ngo);
-        return d3 ;
-  
-      }
-    },[])
-    
-    res = getData();
-
-  }
+    if(admin){
+      console.log(true)
+    }
 
   return (
     <header>
         
         <div>
             <ul className="flex box">
-                <li > <h4> { SelectOccupied(res) } </h4></li>
+                <li > <h4> {[user , admin , ngo]}</h4></li>
                 <li ><a href="#" onClick={onLogout} className="btn">Logout</a></li>
             </ul>
         </div>
