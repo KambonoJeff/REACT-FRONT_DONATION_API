29b27441b7ee3e34367123333dd4bd6d31c89d07
KAMBONO <kambonoj@gmail.com>
2023-07-20 19:54:58 +0300

    Router modification

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -30,18 +30,16 @@ export default function Dashboard() {
 
             <p>Through the selfless act of donation, a life was saved. In the darkest hour, compassion prevailed, providing vital resources and a second chance. It's a reminder that each contribution, no matter how small, holds the power to transform and bring hope where it's needed most.
               <br />
-              <br /></p>
+              <br />
+              
+            <Link className='btn btn-w' to="/users/requests">Requests</Link>
+            <Link className='btn btn-w' to="/users/user">User</Link></p>
 
 
           </div>
           </div>
           <div align='center'>
-          <div className="box mg-t">
-            
-
-            <Link className='btn' to="/users/requests">Requests</Link>
-            <Link className='btn' to="/users/user">User</Link>
-          </div>
+          
           </div>
           
       </div>
