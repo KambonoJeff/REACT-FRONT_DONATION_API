1ee6384d35c751652e62857ca0a799910407bfd3
KAMBONO <kambonoj@gmail.com>
2023-07-24 20:46:59 +0300

    Type of user control development

--- src/components/tables/Food.jsx
+++ src/components/tables/Food.jsx
@@ -66,7 +66,7 @@ const Food = () => {
                             </tr>
                         ))
                     }
-       fbglxblxfnkb
+       
                 </tbody> 
                 </table>  
                 <br />
