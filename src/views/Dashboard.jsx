c6f7a455ab837cdaa2ea999827ff8ecec8b73c9e
KAMBONO <kambonoj@gmail.com>
2023-09-15 23:03:54 +0300

    modified system

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,9 +1,12 @@
 import { Link, Navigate } from 'react-router-dom'
 import image from '../Assets/img/pic3.jpg'
 import image2 from '../Assets/img/pic2.jpg'
-import image3 from '../Assets/img/pic3.jpg'
-import image4 from '../Assets/img/pic4.jpg'
+import image3 from '../Assets/img/opharned.jpg'
+import image4 from '../Assets/img/mulnutrition.jpg'
 import image5 from '../Assets/img/image1.jpg'
+import image6 from '../Assets/img/dirtywater.jpeg'
+import image7 from '../Assets/img/slums.jpg'
+import image8 from '../Assets/img/lackofhospitals.png'
 import { useStateContext } from '../components/contexts/ContextProvider'
 import { useRef, useState } from 'react';
 import axiosClient from '../axios-client';
@@ -67,48 +70,65 @@ export default function Dashboard() {
           </div>
           </form>
           <div className='card-container'>
+            <br />
+            <h3 align='center'>PROVIDE SUPPORT FOR A SPECIFIC AREA OF NEED YOU ARE MOST PASSIONATE ABOUT.</h3>
+            <br />
+            <br />
 				<div className="card">
 				  <img src={image5} alt="Image 1"/>
 				  <div className="details">
-					<h2>React-Front</h2>
-					<p>This is a React oriented project that combines full javaScript,HTML and CSS</p>
+					<h2 align='center'>Extreme Drought</h2>
+					<p align='center'> Extreme drought: parched earth, dwindling resources, and urgent need for conservation.</p>
 				  </div>
 				</div>				
 				<div className="card">
 				  <img src={image4} alt="Image 2"/>
 				  <div className="details">
-					<h2>Python-Django</h2>
-					<p>Python Django is a robust framework that allows flexibilty and economic projects</p>
+					<h2 align='center'>Malnutrition</h2>
+					<p align='center'>Malnutrition harms health, hinders growth, and requires global efforts for eradication</p>
 				  </div>
 				</div>			
 				<div className="card">
 				  <img src={image3} alt="Image 3"/>
 				  <div className="details">
-					<h2>Laravel-PHP</h2>
-					<p>laravel-PHP is a robust framework that allows flexibilty and economic projects</p>
+					<h2 align='center'>Orphaned or Abandoned Children</h2>
+					<p align='center'>Orphaned or abandoned children deserve love, care, and support for a brighter, hopeful future</p>
 				  </div>
 				</div>
 			  </div>
-			  
-          <div className="box">
-          <div className="flex box box mg-t">
-          <div className="image">
-              <img className='img' src={image2} alt="This is an image of a desert" />
-              </div>
-
-            <p>Through the selfless act of donation, a life was saved. In the darkest hour, compassion prevailed, providing vital resources and a second chance. It's a reminder that each contribution, no matter how small, holds the power to transform and bring hope where it's needed most.
-              <br />
-              <br />
-              <div className="flex box mg-t">
+          <div className='card-container'>
+         
+				<div className="card">
+				  <img src={image6} alt="Image 1"/>
+				  <div className="details">
+					<h2 align='center'>Unsafe Drinking Water</h2>
+					<p align='center'> Unsafe drinking water endangers lives, urging investment in clean and accessible water sources for communities worldwide</p>
+				  </div>
+				</div>				
+				<div className="card">
+				  <img src={image8} alt="Image 2"/>
+				  <div className="details">
+					<h2 align='center'>Lack of Medical Care</h2>
+					<p align='center'>"Lack of medical care leads to preventable suffering, emphasizing the importance of accessible healthcare for all</p>
+				  </div>
+				</div>			
+				<div className="card">
+				  <img src={image7} alt="Image 3"/>
+				  <div className="details">
+					<h2 align='center'>Unsafe Housing</h2>
+					<p align='center'>Unsafe housing jeopardizes lives, demanding immediate attention to ensure secure, healthy living conditions for all</p>
+				  </div>
+				</div>
+			  </div>
+        <div className="box">
+        <div className="flex box mg-t">
             
               <Link align='center' className='btn btn-w' to="/users/requests">Requests</Link>
             <Link align='center' className='btn btn-w' to="/users/user">User</Link>
 
           </div>
-          </p>
+          </div>
 
-          </div>
-          </div>
 
           <div className="box">
           <div className="flex box box mg-t">
