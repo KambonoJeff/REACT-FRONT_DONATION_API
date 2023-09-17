d0816218fbf81c5b0e734107b82d0e111cad0f8e
KAMBONO <kambonoj@gmail.com>
2023-09-17 20:00:28 +0300

    modified the system

--- src/components/cards/CompareCards.jsx
+++ src/components/cards/CompareCards.jsx
@@ -9,22 +9,18 @@ const CompareCards = () => {
     const [sum,setSum]=useState([])
     const[a,setA]=useState();
     let navigate = useNavigate();
-    const payload=
-    [
-        
-    ]
+    const payload ={
+        cereals:sum[0],
+        proteins:sum[1],
+        legumes:sum[2],
+        breakfast:sum[3],
+        snacks:sum[4],
 
+    }
     useEffect(()=>{
         // first request
         setLoad(true);
-        axiosClient.get('/food')
-        .then((res)=>{
-            setLoad(false)
-            setSum(res.data)
-          })
-        .catch(err =>{ setLoad(false); console.error(err)});
-        // second request
-        setLoad(true);
+        console.log('First request succesful!')
         axiosClient.get('/requests/compare')
         .then(({data})=>{
             setLoad(false)
@@ -34,19 +30,43 @@ const CompareCards = () => {
             setLoad(false)
             console.log('error occured when making the request', err.response)
         });
-        //third request
-        setLoad(true)
-        axiosClient.post('/requests/diffrence',payload)
-        .then(({data})=>{
+    },[])
+    useEffect(()=>{
+         // second request
+        setLoad(true);
+        console.log('second request succesful!')
+
+        axiosClient.get('/food')
+        .then((res)=>{
             setLoad(false)
-            console.log(data)
-        })
-        .catch((err)=>{
-            setLoad(false)
-            console.log('An error occured when get the diffrence', err.response)
-        })
+            setSum(res.data)
+          })
+        .catch(err =>{ setLoad(false); console.error(err)});
+    },[])
+    useEffect(()=>{
+         //third request
+         setLoad(true)
+         console.log('This the diffrence payload',payload)
+         axiosClient.post('/requests/diffrence',payload)
+         .then(({data})=>{
+             setLoad(false)
+             console.log(data)
+         })
+         .catch((err)=>{
+             setLoad(false)
+             console.log('An error occured when get the diffrence', err.response)
+         });
+    },[])
+
+    useEffect(()=>{
+        
+       
+
+       
         //fourth request
         setLoad(true)
+        console.log('fourth request succesful!')
+
         axiosClient.get('/sumfood')
         .then((res)=>{
         setLoad(false)
