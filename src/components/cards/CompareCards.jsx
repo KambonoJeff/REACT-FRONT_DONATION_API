d642f51d2d3e424a415c0f6932bc767d149db996
KAMBONO <kambonoj@gmail.com>
2023-09-17 12:41:34 +0300

    modified the system

--- src/components/cards/CompareCards.jsx
+++ src/components/cards/CompareCards.jsx
@@ -37,42 +37,72 @@ const CompareCards = () => {
 
   return (
     <>
-      <div className="grid box">
+    <br />
+    <br />
+    <br />
+    <br />
+      <div align='center' className="grid box">
         <div className="grid box">
-            <h2>Details</h2>
             <div className="flex box">
             <div className="card-container">
-            <h3>Donations</h3>
-            <div className="grid">
-            <div class="card3 ">
-         <div class="amount">Kgs {sum[0]}</div> 
-        <div class="title"> Cereals </div>
-    </div>
-
-    <div class="card3 ">
-         <div class="amount">Kgs {sum[1]}</div> 
-        <div class="title"> Proteins </div>
-    </div>
-    <div class="card3 ">
-         <div class="amount">Kgs {sum[2]}</div> 
-        <div class="title"> Legumes </div>
-    </div>
-    <div class="card3 ">
-         <div class="amount">Kgs {sum[3]}</div> 
-        <div class="title"> Breakfast </div>
-    </div>
-    <div class="card3 ">
-         <div class="amount">Kgs {sum[4]}</div> 
-        <div class="title"> Snacks </div>
-    </div>
-    <div class="card3 ">
-         <div class="amount">${sum[5]}</div> 
-        <div class="title"> Cash </div>
-    </div> 
-            </div>
+                <h3>Donations</h3>
+                <div className="grid">
+                <div class="card3 ">
+            <div class="amount">Kgs {sum[0]}</div> 
+            <div class="title"> Cereals </div>
         </div>
+
+        <div class="card3 ">
+            <div class="amount">Kgs {sum[1]}</div> 
+            <div class="title"> Proteins </div>
+        </div>
+        <div class="card3 ">
+            <div class="amount">Kgs {sum[2]}</div> 
+            <div class="title"> Legumes </div>
+        </div>
+        <div class="card3 ">
+            <div class="amount">Kgs {sum[3]}</div> 
+            <div class="title"> Breakfast </div>
+        </div>
+        <div class="card3 ">
+            <div class="amount">Kgs {sum[4]}</div> 
+            <div class="title"> Snacks </div>
+        </div>
+        <div class="card3 ">
+            <div class="amount">${sum[5]}</div> 
+            <div class="title"> Cash </div>
+        </div> 
+                </div>
+            </div>
         <div className="card-container">
             <h3>Requests</h3>
+            <div className="grid">
+                <div class="card3 ">
+            <div class="amount negative">Kgs {sum[0]}</div> 
+            <div class="title"> Cereals </div>
+        </div>
+
+        <div class="card3 ">
+            <div class="amount negative">Kgs {sum[1]}</div> 
+            <div class="title"> Proteins </div>
+        </div>
+        <div class="card3 ">
+            <div class="amount negative">Kgs {sum[2]}</div> 
+            <div class="title"> Legumes </div>
+        </div>
+        <div class="card3 ">
+            <div class="amount negative">Kgs {sum[3]}</div> 
+            <div class="title"> Breakfast </div>
+        </div>
+        <div class="card3 ">
+            <div class="amount negative">Kgs {sum[4]}</div> 
+            <div class="title"> Snacks </div>
+        </div>
+        <div class="card3 ">
+            <div class="amount negative">${sum[5]}</div> 
+            <div class="title"> Cash </div>
+        </div> 
+                </div>
         </div>
         </div>
         </div>
